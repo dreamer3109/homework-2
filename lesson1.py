1fa72023a3f99f26b734e1f87a9eae39c7d203b4
goerge <134689050+dreamer3109@users.noreply.github.com>
2023-05-29 11:42:09 -0700

    Add files via upload

---
+++ lesson1.py
@@ -0,0 +1,30 @@
+
+name = "George"
+surname = "Memarnidze"
+nickname = "mekesh"
+height = "179.5"
+weight = "65.5"
+age = "20"
+brown_eyes = " True"
+athletick_body = "False"
+is_soldier = "True"
+is_future_millioner = "True"
+loves_dogs = "True"
+
+
+print(name + " " + surname + " " + nickname + " " + age + " " + str(height) + " " + str(weight) + " " + brown_eyes + " " + athletick_body + " " + loves_dogs + " " + is_soldier + " " + is_future_millioner)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
