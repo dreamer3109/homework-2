da489b401a966f265280b5de5d90bd6e7661b033
goerge <134689050+dreamer3109@users.noreply.github.com>
2023-06-13 07:05:54 -0700

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,41 @@
+name = "Giorgi"     #1
+print(len(name))
+
+
+my_sentence = """Hi,my name is George,
+I am from Tbilisi                   #2
+I am soldier"""                  
+print(my_sentence)
+
+if 5>3:
+    print("hello")      #3
+
+full_name = "giorgi memarnidze"   #6+10+1(space)
+print(len(full_name))                #yvela str shegvidzlia mivichniot siad  mag(g=0,i=2,0=3...)
+print(full_name[0])
+print("z" in full_name)
+print("l" not in full_name)
+
+
+
+full_name = "giorgi memarnidze"
+print(full_name[3:11])
+print(full_name[3:11:2])
+print(full_name[:6])
+print(full_name[6:])
+
+print(full_name[-1])
+print(full_name[-1:-6:-2])
+print(full_name[-5] + "ature")
+
+name2 = "giorgi"
+print(name2.upper())
+print(name2.lower())
+
+name3 = "     gio    "
+print(name3.replace(" ", "#"))
+
+
+
+
+
